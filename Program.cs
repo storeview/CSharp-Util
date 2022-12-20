8123def115ccef2af50cd785039887db8829d9f2
li-linfang <lilnfh25@163.com>
2022-12-21 01:01:31 +0800

    1

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using CSharp_Util.Test.MqttTestServer;
 using CSharp_Util.Test.WCF.Basic1;
 using CSharp_Util.Test.WCF.Basic2;
 using CSharp_Util.Util;
+using CSharpUtil;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,7 +17,13 @@ namespace CSharp_Util
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             MqttTest1.Run();
+=======
+            FileUtils.CreatZip("D:\\Windows数据移动到此文件夹\\Desktop\\333\\333.exe", "D:\\Windows数据移动到此文件夹\\Desktop\\Zips\\333.zip");
+
+            //Run2.Run();
+>>>>>>> Stashed changes
         }
     }
 }
